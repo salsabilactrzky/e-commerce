22f653b92cc222aba7e5045489c2e9b393effba1
Wh0Rv <salsabilacf04@icloud.com>
2024-09-11 11:42:30 +0700

    ok

--- main/tests.py
+++ main/tests.py
@@ -1,26 +1,48 @@
 from django.test import TestCase, Client
 from django.utils import timezone
+from django.urls import reverse
 from .models import Product
+
+# class mainTest(TestCase):
+#     def test_main_url_is_exist(self):
+#         response = Client().get('')
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_main_using_main_template(self):
+#         response = Client().get('')
+#         self.assertTemplateUsed(response, 'main.html')
+
+#     def test_nonexistent_page(self):
+#         response = Client().get('/skibidi/')
+#         self.assertEqual(response.status_code, 404)
+
+#     def test_strong_mood_user(self):
+#         now = timezone.now()
+#         mood = Product.objects.create(
+#           mood="LUMAYAN SENANG",
+#           time = now,
+#           feelings = "senang sih, cuman tadi baju aku basah kena hujan :(",
+#           mood_intensity = 8,
+#         )
+#         self.assertTrue(mood.is_mood_strong)
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
-        response = Client().get('')
+        response = Client().get(reverse('main:show_main'))
         self.assertEqual(response.status_code, 200)
 
     def test_main_using_main_template(self):
-        response = Client().get('')
+        response = Client().get(reverse('main:show_main'))
         self.assertTemplateUsed(response, 'main.html')
 
     def test_nonexistent_page(self):
         response = Client().get('/skibidi/')
         self.assertEqual(response.status_code, 404)
 
-    def test_strong_mood_user(self):
-        now = timezone.now()
-        mood = Product.objects.create(
-          mood="LUMAYAN SENANG",
-          time = now,
-          feelings = "senang sih, cuman tadi baju aku basah kena hujan :(",
-          mood_intensity = 8,
-        )
-        self.assertTrue(mood.is_mood_strong)
+    def test_product_is_sold(self):
+        self.product.stock = 0
+        self.product.save()
+        self.assertTrue(self.product.is_sold)
+
+    def test_product_is_not_sold(self):
+        self.assertFalse(self.product.is_sold)
--- main/views.py
+++ main/views.py
@@ -4,13 +4,12 @@ def show_main(request):
     context = {
         'name' : 'Vintage Bag',
         'price': 'Rp1500000',
-        'description': 'Tas vintage branded bekas yang masih bagus',
+        'description': 'Tas vintage branded bekas',
         'category' : 'Tas',
+        'brand' : 'Versace',
         'condition' : 'Good condition, tidak ada cacat',
         'stock' : '1',
-        'image' : '',
-        'brand' : 'Louis Vuitton',
-        'sold' : 'Sold',
+
     }
 
     return render(request, "main.html", context)
