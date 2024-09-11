4c56f7d089b91c42861fc82837449427190aa1b3
Wh0Rv <salsabilacf04@icloud.com>
2024-09-11 11:49:09 +0700

    rapihin

--- main/tests.py
+++ main/tests.py
@@ -3,29 +3,6 @@ from django.utils import timezone
 from django.urls import reverse
 from .models import Product
 
-# class mainTest(TestCase):
-#     def test_main_url_is_exist(self):
-#         response = Client().get('')
-#         self.assertEqual(response.status_code, 200)
-
-#     def test_main_using_main_template(self):
-#         response = Client().get('')
-#         self.assertTemplateUsed(response, 'main.html')
-
-#     def test_nonexistent_page(self):
-#         response = Client().get('/skibidi/')
-#         self.assertEqual(response.status_code, 404)
-
-#     def test_strong_mood_user(self):
-#         now = timezone.now()
-#         mood = Product.objects.create(
-#           mood="LUMAYAN SENANG",
-#           time = now,
-#           feelings = "senang sih, cuman tadi baju aku basah kena hujan :(",
-#           mood_intensity = 8,
-#         )
-#         self.assertTrue(mood.is_mood_strong)
-
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
         response = Client().get(reverse('main:show_main'))
